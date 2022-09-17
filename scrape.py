305c3216c31abb61b3c19b805aaacd39ec71217e
Ayaka Mikazuki <ayaka@mail.shn.hk>
2022-09-17 22:29:02 +0800

    Update list

--- scrape.py
+++ scrape.py
@@ -1,13 +1,9 @@
 import json
 import re
 import requests
+import logging
 
-def determine_video_url(content):
-    if content:
-        match = re.search(r'<video src="([^"]+)"[^<>]+>', content)
-        if match:
-            return match.group(1)
-    return ''
+logging.basicConfig(level=logging.DEBUG)
 
 url = 'http://hongqi.wengegroup.com:9001/search/searchUserSprint'
 
@@ -22,16 +18,38 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Linux; Android 9; INE-AL00 Build/HUAWEIINE-AL00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/104.0.5112.69 Mobile Safari/537.36 Html5Plus/1.0 (Immersed/35.294117)',
 }
 
-for page_num in range(6):  # range(124, 300)
+proxies = {
+    'http': 'socks5://127.0.0.1:1081/',
+    'https': 'socks5://127.0.0.1:1081/',
+}
+
+def read_existing_list(filename: str='list.csv') -> dict:
+    d = {}
+    with open(filename, encoding='utf-8') as f:
+        for line in f:
+            title, post_url, video_url = line.rstrip('\n').split(',')
+            d[title] = post_url, video_url
+    return d
+
+def get_video_url_from_content(content: str) -> str:
+    if content:
+        match = re.search(r'<video src="([^"]+)"[^<>]+>', content)
+        if match:
+            return match.group(1)
+    return ''
+
+def request_search(page: int) -> dict:
+    d = {}
+
     payload = {
         'appInfoId': '32',
         'keyword': '《文昌新闻》海南话',
         'loginUserId': '',
-        'pageNum': page_num,
+        'pageNum': page,
         'pageSize': 10,
     }
 
-    r = requests.post(url, data=json.dumps(payload), headers=headers)
+    r = requests.post(url, data=json.dumps(payload), headers=headers, proxies=proxies)
     r.raise_for_status()
     obj = r.json()
     assert obj['success']
@@ -52,6 +70,19 @@ for page_num in range(6):  # range(124, 300)
 
             content = item['content']
 
-            video_url = determine_video_url(content) or item['properties']['accessUrl']
+            video_url = get_video_url_from_content(content) or item['properties']['accessUrl']
 
-            print(title, post_url, video_url, sep=',')
+            d[title] = post_url, video_url
+
+    return d
+
+def main():
+    final_data = {**{k: v for page in range(20) for k, v in request_search(page).items()}, **read_existing_list()}
+    final_data = sorted((title, post_url, video_url) for title, (post_url, video_url) in final_data.items())
+
+    with open('list2.csv', 'w', encoding='utf-8') as f:
+        for title, post_url, video_url in final_data:
+            print(title, post_url, video_url, sep=',', file=f)
+
+if __name__ == '__main__':
+    main()
